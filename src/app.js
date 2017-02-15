a7ba981985ad6dd0220daab3e05a4e4584c4c274
ricardotulio <ricardo.tulio@fatec.sp.gov.br>
2017-02-15 14:07:26 -0200

    Fix eslit erros

--- src/app.js
+++ src/app.js
@@ -2,6 +2,4 @@ import server from './http/server'
 
 const HTTP_PORT = process.env.HTTP_PORT || 8888
 
-server.listen(HTTP_PORT, () => {
-  console.log(`Listing on port ${HTTP_PORT}`)
-})
+server.listen(HTTP_PORT)
